219e5981e31ed40cd3ccf8bf5a66f46e9cad1aa2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-04 15:45:39 +0200

    Created a custom button reusable component

---
+++ src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import styles from "./custom-button.module.scss";
+type Props = {
+  title: string;
+  disabled?: boolean;
+  type?: "button" | "reset" | "submit" | undefined;
+  customClassName: string;
+  action: () => void;
+};
+
+function CustomButton({
+  title,
+  disabled,
+  type,
+  customClassName,
+  action,
+}: Props) {
+  return (
+    <button
+      disabled={disabled}
+      className={`${styles.customButton} ${styleFactory(customClassName)}`}
+      type={type}
+      onClick={action}
+    >
+      {title}
+    </button>
+  );
+}
+
+//gets the button translated button style based on input
+function styleFactory(enteredStyle: string): string {
+  switch (enteredStyle) {
+    case "style1":
+      return styles.customStyle1;
+    default:
+      return ""; //fallback
+  }
+}
+
+export default CustomButton;
