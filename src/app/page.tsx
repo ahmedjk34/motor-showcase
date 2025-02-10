779da8095310763c4156a3251a50c9a8ca8caf5f
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-10 20:55:29 +0200

    Added Catalog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/pages/home-page.module.scss";
 import Hero from "./_homeComponents/Hero";
+import Catalog from "./_homeComponents/Catalog";
 
 export default function Home() {
   return (
     <div className={styles.homePage}>
       <Hero />
+      <Catalog />
     </div>
   );
 }
