f23b9d3d40d275814ecf4c930a75361e3ff44e02
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-13 21:07:42 +0200

    Displayed the cars in the catalog

--- src/app/_homeComponents/Catalog.tsx
+++ src/app/_homeComponents/Catalog.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import styles from "../../styles/pages/home-page.module.scss";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import CustomFilter from "@/components/CustomFilter/CustomFilter";
+import { getCars } from "@/API";
+import CarCard from "@/components/CarCard/CarCard";
 type Props = {};
 
-function Catalog({}: Props) {
+async function Catalog({}: Props) {
+  const allCars = await getCars();
   return (
     <div className={styles.catalog}>
       <div className={styles.titleHolder}>
@@ -17,6 +20,19 @@ function Catalog({}: Props) {
           <CustomFilter title="fuel" />
           <CustomFilter title="year" />
         </div>
+      </div>
+      <div>
+        {allCars ? (
+          <div className={styles.carsWrapper}>
+            {allCars.map((car) => (
+              <CarCard car={car} />
+            ))}
+          </div>
+        ) : (
+          <div className={styles.noCarsFoundSection}>
+            <h1>No Cars Found</h1>
+          </div>
+        )}
       </div>
     </div>
   );
