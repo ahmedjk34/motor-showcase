4ad1f3dc91abf83b394c660d7281567f58e7c165
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-13 21:08:18 +0200

    Created search filter component

---
+++ src/components/CustomFilter/CustomFilter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./custom-filter.module.scss";
+type Props = {};
+
+function CustomFilter({}: Props) {
+  return <div>CustomFilter</div>;
+}
+
+export default CustomFilter;
