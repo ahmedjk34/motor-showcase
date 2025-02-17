0c436f18dabc24b3553f5c3425552b4472929f39
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-17 12:51:18 +0200

    Added support for small screens (final commit)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -9,7 +9,7 @@ function Nav({}: Props) {
   return (
     <nav className={styles.nav}>
       <Link href={"/"} className={styles.logoContainer}>
-        <Image src="/logo.png" alt="logo" height={30} width={200} />
+        <Image src="/logo.png" alt="logo" height={45} width={200} />
       </Link>
       <CustomButton title="Sign in" customClassName="style2" />
     </nav>
