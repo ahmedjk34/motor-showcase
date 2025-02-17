5f7b702ebbb986f4b5647687011c6fc408463959
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-17 12:17:10 +0200

    Code cleanup (final commit)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "../styles/pages/home-page.module.scss";
 import Hero from "./_homeComponents/Hero";
 import Catalog from "./_homeComponents/Catalog";
