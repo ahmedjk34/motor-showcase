c1427064b002e8083aac357fb5e9df79561667e4
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-19 20:45:49 +0200

    Moved the API Car data to be local instead of relying on an external API

--- src/API.ts
+++ src/API.ts
@@ -1,35 +1,23 @@
-import axios from "axios";
 import { Car, SearchFilters } from "./Types";
+import carsData from "./carData";
 
-const headers = {
-  "x-rapidapi-key": "c110a841aemsh730fe467f8b41c9p1fa73cjsn71210b6131cd", //HIDE THE KEY
-  "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
-};
+export function getCars(searchFilters: SearchFilters): Car[] | null {
+  const { model, fuel, manufacturer, year } = searchFilters;
 
-const options = {
-  method: "GET",
-  url: "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
-  headers: {
-    "x-rapidapi-key": process.env.API_KEY,
-    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
+  const filteredCars = carsData.filter((car) => {
+    return (
+      (!model || car.model.toLowerCase().includes(model.toLowerCase())) &&
+      (!fuel || car.fuel_type.toLowerCase() === fuel.toLowerCase()) &&
+      (!manufacturer ||
+        car.make.toLowerCase().includes(manufacturer.toLowerCase())) &&
+      (!year || car.year === year)
+    );
+  });
+  console.log(filteredCars);
 
-export async function getCars(
-  searchFilters: SearchFilters
-): Promise<Car[] | null> {
-  try {
-    const { model, fuel, manufacturer, year } = searchFilters;
-    const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars";
-    const params = { model, fuel_type: fuel, manufacturer, year };
-    const response = await axios.get(url, { headers, params });
-    const data = response.data;
-    return data.length == 0 ? null : data; //if the data list is empty, return it as null
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+  return filteredCars.length === 0 ? null : filteredCars;
 }
+
 export const getCarImageURL = (car: Car, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
